d8c8b2fcefbcba950b784741eb22219025fabf28
Kubra-Yesim-Tatli <kyipoyraz@gmail.com>
2024-12-12 18:44:30 +0300

    SignUpPage SignupForm olarak değiştirildi ve düzenlemeler yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import PageContent from "./layout/PageContent";
 import HomePage from "./Pages/HomePage";
-import ProductCard from "./components/ProductCard";
-import SignUpPage from "./components/SignUpPage"; // SignUpPage'i import ediyoruz
+
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Header />
         <Switch> {/* Switch ile sadece bir route'un render edilmesini sağlıyoruz */}
           <Route exact path="/" component={HomePage} /> {/* HomePage için bir route */}
-          <Route path="/signup" component={SignUpPage} /> {/* /signup için SignUpPage route'u */}
+          <Route path="/signup" component={SignupForm} /> {/* /signup için SignUpPage route'u */}
         </Switch>
         <Footer />
       </PageContent>
