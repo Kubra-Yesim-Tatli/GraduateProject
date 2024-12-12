434ab046c310b245ad53955caf40300a91f25117
Kubra-Yesim-Tatli <kyipoyraz@gmail.com>
2024-12-12 14:26:24 +0300

    ProductCard HomePage'e eklendi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Slider from "../components/Slider";
 import EditorsPickSection from "../components/EditorsPickSection";
+import ProductCard from "../components/ProductCard";
 const HomePage = () => {
   return (
     <div>
        
       <Slider />
       <EditorsPickSection />
+      <ProductCard/>
     </div>
   );
 };
